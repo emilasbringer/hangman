1f920cb48fc6fa05c2e9525c869ffcbb3a117879
emilasbringer <FattyRatty@users.noreply.github.com>
2021-01-16 01:30:43 +0100

    Array för att visa hur långt ordet är + inputdialog för gissningen

--- hangman.java
+++ hangman.java
@@ -1,17 +1,28 @@
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class hangman {
     public static void main (String[] arg) {
 
         String secretword = randomWordGenerator();
+        String guess;
         int wordlength = secretword.length();
+        int ToFlength = secretword.length();
         int i = 0;
+        int life = 10;
 
         char letters[] = new char[wordlength];
+        char trueorfalse[] = new char [ToFlength];
+        char empty = '_';
 
         while(i < wordlength) {
           letters[i] = secretword.charAt(i);
+          trueorfalse[i] = empty;
           i++;
         }
 
+        guess = JOptionPane.showInputDialog("You have " + life + " guesses remaining! " + "\n" + Arrays.toString(trueorfalse));
 
 
 
